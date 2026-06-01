6a14a1f25a8932997e64cbfa5a312239444df8be
Lintang <lintangwaladi@gmail.com>
2026-06-01 18:59:31 +0700

    feat: implement SessionManager and BaseController

---
+++ src/main/java/com/mycompany/pengelolaanproject/core/BaseController.java
@@ -0,0 +1,8 @@
+package com.mycompany.pengelolaanproject.core;
+
+/**
+ * Abstract base class for all controllers in the system.
+ */
+public abstract class BaseController {
+    // General methods or properties shared by all controllers can be added here.
+}
